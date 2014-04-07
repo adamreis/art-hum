9f3c426d9ed75ba65fbfb326826c32158b85e56a
Adam Reis <adamhreis@gmail.com>
2014-04-07 04:26:28 -0400

    made some progress, now just need to route tags

--- app.js
+++ app.js
@@ -8,20 +8,23 @@ var express = require('express'),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
     path = require('path'),
-    mustache = require('mustache-express');
+    mustache = require('mustache-express'),
+    mongoose = require('mongoose');
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('mustache', mustache());
-// app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.favicon(path.join(__dirname,'/public/images/favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+  // all environments
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.engine('mustache', mustache());
+  // app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.favicon(path.join(__dirname,'/public/images/favicon.ico')));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
 
 // console.log('expected favicon path: ' + path.join(__dirname,'/public/images/favicon.ico'));
 
@@ -31,16 +34,51 @@ app.use(express.static(path.join(__dirname, 'public')));
 // }
 
 /**
+ * Set up mongo
+ */
+
+mongoose.connect('mongodb://localhost/arthum');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+  console.log('db open!');
+
+  var mapSchema = mongoose.Schema({
+    // _id: mongoose.Schema.Types.ObjectId,
+    theme: String,
+    tags: []
+  });
+
+  var Map = mongoose.model('Map', mapSchema);
+  // var map = new Map({
+  //   // _id:1,
+  //   theme:'Leisure',
+  //   tags: []
+  // });
+  //
+  // map.save(function (err, map) {
+  //   if (err) return console.error(err);
+  //   console.log('save for map with theme ' + map.theme + ' successful');
+  // });
+});
+
+
+/**
  * Serve my code
  */
 
-app.get('/', function(req, res, next){
+app.get('/', function (req, res, next){
   res.render('index.mustache', { title: 'Express' });
 });
 
-app.get('/map', function(req, res, next){
-  res.send('hey thurr')
+app.get('/map', function (req, res, next){
+  res.render('mapview.mustache', {theme: req.query.theme});
 });
+
+// app.get('/uploadform.html', function (req, res, next) {
+//   res.render('uploadform.html');
+// });
 
 /**
  * Socket.IO stuff
@@ -58,14 +96,9 @@ io.sockets.on('connection', function(socket) {
   console.log('connection # '+socket.id);
 
   socket.on('click', function(msg) {
-    try {
-      var pos = JSON.parse(msg);
-    } catch (e) {
-      return;
-    }
-    positions[socket.id] = pos;
-    io.sockets.emit('position', JSON.stringify({pos: pos, id: socket.id}));
-    socket.emit('hi');
+    msg = JSON.parse(msg);
+    positions[socket.id] = msg;
+    io.sockets.emit('position', JSON.stringify({pos: msg.pos, id: socket.id, link: msg.link}));
   });
 
   socket.on('disconnect', function () {
